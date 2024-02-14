fe115e603fb46512dd32cc5d0bff44c3601000ab
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-02-14 12:47:18 +0530

    Update preprocessing_and_objectdetection.py

--- Model Files/preprocessing_and_objectdetection.py
+++ Model Files/preprocessing_and_objectdetection.py
@@ -18,16 +18,16 @@ os.makedirs(segmented_dir, exist_ok=True)
 for filename in os.listdir(dataset_dir):
     # Check if the file is an image
     if filename.endswith('.png') or filename.endswith('.jpg'):
-        # Construct the full file path
+        # Constructing the full file path
         filepath = os.path.join(dataset_dir, filename)
         
         # Load the image
         img = cv2.imread(filepath, 0)
 
-        # Apply the median filter
+        # Applying median filter
         median_filtered_img = cv2.medianBlur(img, ksize=3)
 
-        # Normalize the image
+        # Normalizing the image
         normalized_img = cv2.normalize(median_filtered_img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 
         # Reshape the image to be a list of pixel intensities
@@ -35,11 +35,11 @@ for filename in os.listdir(dataset_dir):
 
         adaptive_thresh_img = cv2.adaptiveThreshold(normalized_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
 
-        # Perform K-means clustering
-        kmeans = KMeans(n_clusters=7, random_state=0).fit(pixels)
+        # Performing K-means clustering 
+        kmeans = KMeans(n_clusters=50, random_state=0).fit(pixels)
 
         # Replace each pixel with its cluster center
         segmented_img = kmeans.cluster_centers_[kmeans.labels_].reshape(normalized_img.shape).astype(np.uint8)
 
-        # Save the segmented image
+        # Saving the segmented image
         cv2.imwrite(os.path.join(segmented_dir, 'segmented_' + filename), segmented_img)
