7da8f975337a40ce927f96115489eb3310be41f8
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-04 11:58:31 +0530

    Update preparing_data.py

--- Model Files/preparing_data.py
+++ Model Files/preparing_data.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 # Directory containing segmented images
@@ -23,28 +22,28 @@ for filename in sorted(os.listdir(segmented_dir)):
         # Constructing the full file path of segmented image 
         filepath = os.path.join(segmented_dir, filename)
         
-        # Loading the segmented image as a numpy array and 
-        # append to the data list
+        # Loading the segmented image as a numpy array and append to data list if not empty 
         img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-        data.append(img)
+        if img.size > 0:
+            data.append(img)
 
-# Convert the list to a numpy array
+# Convert list to numpy array 
 data = np.array(data)
 
 # Creating Sequences 
 seq_length = 10 
-sequences = [data[i:i+seq_length] for i in range(data.shape[0]-seq_length+1)]
+sequences = [data[i:i+seq_length] for i in range(data.shape[0]-seq_length)]
 
-# Normalizing Data (0-1 range)
+# Normalizing Data (0-1 range) 
 normalized_sequences = [(seq - np.min(seq)) / (np.max(seq) - np.min(seq)) for seq in sequences]
 
 # Split Data into Training and Testing Sets 
 train_data, test_data = train_test_split(normalized_sequences, test_size=0.2)
 
-# Reshape Data to fit model input dimensions 
-train_data = np.reshape(train_data, (len(train_data), seq_length, img.shape[0], img.shape[1], 1))
-test_data = np.reshape(test_data, (len(test_data), seq_length, img.shape[0], img.shape[1], 1))
+# Reshape Data fit model input dimensions  
+train_data = np.reshape(train_data, (len(train_data), seq_length,) + img.shape + (1,))
+test_data = np.reshape(test_data, (len(test_data), seq_length,) + img.shape + (1,))
 
-# Saving prepared data to new folder (.npy files)
+# Saving prepared data new folder (.npy files)  
 np.save(os.path.join(prepared_data_dir,'train_data.npy'), train_data)
 np.save(os.path.join(prepared_data_dir,'test_data.npy'), test_data)
