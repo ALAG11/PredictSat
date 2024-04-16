b3eff7f6f9b3f1d398cbc68928284c3fa511696c
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-04-16 13:03:40 +0530

    Update ConvolutionalLSTMmodel.py
    Code modified to run only on CPU computation (without GPU's), updated the batch size, added custom data generator and modified the code to run in less memory.

--- Model Files/ConvolutionalLSTMmodel.py
+++ Model Files/ConvolutionalLSTMmodel.py
@@ -1,9 +1,14 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 import numpy as np
 import os
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import ConvLSTM2D, BatchNormalization, Conv2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.layers import Input
 
 # Specifying the directory where the .npy prepared data files are located
 prepared_data_dir = '/home/trg1/alok/model/PreparedData'
@@ -16,28 +21,44 @@ test_data = np.load(os.path.join(prepared_data_dir,'test_data.npy'))
 x_train, y_train = train_data[:, :-1], train_data[:, -1]
 x_test, y_test = test_data[:, :-1], test_data[:, -1]
 
-# Add a time dimension to data
-x_train = np.expand_dims(x_train, axis=2)  # Now shape is (samples, seq_length, 1, rows, cols, channels)
-x_test = np.expand_dims(x_test, axis=2)  # Now shape is (samples, seq_length, 1, rows, cols, channels)
-
 # Get the input shape from the training data
-input_shape = x_train.shape[2:]
+input_shape = x_train.shape[1:]
 
 # Defining the model
+input_layer = Input(shape=input_shape)
 model = Sequential()
-model.add(ConvLSTM2D(filters=64, kernel_size=(3, 3), return_sequences=True, activation='relu', input_shape=input_shape))
-model.add(BatchNormalization())
-model.add(ConvLSTM2D(filters=64, kernel_size=(3, 3), return_sequences=False, activation='relu'))
-model.add(BatchNormalization())
+model.add(ConvLSTM2D(filters=16, kernel_size=(3, 3), return_sequences=True, activation='relu', padding='same'))
+model.add(ConvLSTM2D(filters=16, kernel_size=(3, 3), return_sequences=False, activation='relu', padding='same'))
 model.add(Conv2D(filters=1, kernel_size=(3, 3), padding='same', activation='sigmoid'))  # Output layer
+model = tf.keras.Model(inputs=input_layer, outputs=model(input_layer))
+
 
 # Compiling the model
 opt = Adam(learning_rate=0.001)
 model.compile(optimizer=opt, loss='mse') 
 
 # Callbacks Functions
-checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='min', save_best_only=True)
+checkpoint = ModelCheckpoint('best_model.keras', monitor='val_loss', mode='min', save_best_only=True)
 earlystop = EarlyStopping(monitor='val_loss', patience=5)
 
-# Training the model
-model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test), callbacks=[checkpoint, earlystop])  
+# Custom data generator
+class CustomDataGen(tf.keras.utils.Sequence):
+    def __init__(self, x_data, y_data, batch_size):
+        self.x_data = x_data
+        self.y_data = y_data
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil(len(self.x_data) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_x = self.x_data[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y_data[idx * self.batch_size:(idx + 1) * self.batch_size]
+        return np.array(batch_x), np.array(batch_y)
+
+# Using the custom data generator
+train_gen = CustomDataGen(x_train, y_train, batch_size=1)
+test_gen = CustomDataGen(x_test, y_test, batch_size=1)
+
+# Training the model with reduced batch size and using custom generator
+model.fit(train_gen, epochs=100, validation_data=test_gen, callbacks=[checkpoint, earlystop], batch_size=1)
