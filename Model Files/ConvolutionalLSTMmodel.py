a55b0f97f2bc76e45e64501e80c116886ec5d182
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-06-02 16:02:27 +0530

    Various functionalities added in code
    Added code to reshape input .npy files dataset and removed unusable packages import statements also added one new layer to model definition and added the functionality of saving the predicted images and the model.

--- Model Files/ConvolutionalLSTMmodel.py
+++ Model Files/ConvolutionalLSTMmodel.py
@@ -2,24 +2,27 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 import numpy as np
-import os
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import ConvLSTM2D, BatchNormalization, Conv2D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import ConvLSTM2D, Conv2D, Input
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.keras.layers import Input
+import cv2
 
 # Specifying the directory where the .npy prepared data files are located
 prepared_data_dir = '/home/trg1/alok/model/PreparedData'
 
 # Loading the training and testing data
-train_data = np.load(os.path.join(prepared_data_dir,'train_data.npy'))
-test_data = np.load(os.path.join(prepared_data_dir,'test_data.npy'))
+train_data = np.load(os.path.join(prepared_data_dir, 'train_data.npy'))
+test_data = np.load(os.path.join(prepared_data_dir, 'test_data.npy'))
 
 # Splitting the data into input and target data
 x_train, y_train = train_data[:, :-1], train_data[:, -1]
 x_test, y_test = test_data[:, :-1], test_data[:, -1]
+
+# Reshape the input data
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 512, 512, 3)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 512, 512, 3)
 
 # Get the input shape from the training data
 input_shape = x_train.shape[1:]
@@ -29,13 +32,14 @@ input_layer = Input(shape=input_shape)
 model = Sequential()
 model.add(ConvLSTM2D(filters=16, kernel_size=(3, 3), return_sequences=True, activation='relu', padding='same'))
 model.add(ConvLSTM2D(filters=16, kernel_size=(3, 3), return_sequences=False, activation='relu', padding='same'))
-model.add(Conv2D(filters=1, kernel_size=(3, 3), padding='same', activation='sigmoid'))  # Output layer
-model = tf.keras.Model(inputs=input_layer, outputs=model(input_layer))
+model.add(Conv2D(filters=3, kernel_size=(3, 3), padding='same', activation='sigmoid'))  # Output layer with 3 filters
 
+# Assign the input layer to the model
+model = Model(inputs=input_layer, outputs=model(input_layer))
 
 # Compiling the model
 opt = Adam(learning_rate=0.001)
-model.compile(optimizer=opt, loss='mse') 
+model.compile(optimizer=opt, loss='mse')
 
 # Callbacks Functions
 checkpoint = ModelCheckpoint('best_model.keras', monitor='val_loss', mode='min', save_best_only=True)
@@ -60,5 +64,29 @@ class CustomDataGen(tf.keras.utils.Sequence):
 train_gen = CustomDataGen(x_train, y_train, batch_size=1)
 test_gen = CustomDataGen(x_test, y_test, batch_size=1)
 
-# Training the model with reduced batch size and using custom generator
-model.fit(train_gen, epochs=100, validation_data=test_gen, callbacks=[checkpoint, earlystop], batch_size=1)
+# Training the model
+history = model.fit(train_gen, epochs=8, validation_data=test_gen, callbacks=[checkpoint, earlystop], batch_size=1)
+
+# Debugging: Print training history to monitor loss
+print(history.history)
+
+# Load the best saved model
+best_model = tf.keras.models.load_model('best_model.keras')
+
+# Define the directory to save the predicted images
+predicted_images_dir = '/home/trg1/alok/model/'
+
+# Define a function to generate predictions
+def generate_predictions(model, input_sequences):
+    # Make predictions
+    predictions = model.predict(input_sequences)
+    # Rescale predictions to the range 0-255
+    predictions = (predictions * 255).astype(np.uint8)
+    return predictions
+
+# Example usage: generate predictions for test data
+predicted_images = generate_predictions(best_model, x_test)
+
+# Save the predicted images
+for i, pred_image in enumerate(predicted_images):
+    cv2.imwrite(os.path.join(predicted_images_dir, f'predicted_image_{i}.png'), pred_image)
